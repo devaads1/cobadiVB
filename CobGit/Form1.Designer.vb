bbeab4ae683b4be891fe919c50be995f62f89a1a
Biasa <Biasa@BIASA-PC-070>
2018-11-02 16:02:45 +0800

    aaaa

--- CobGit/Form1.Designer.vb
+++ CobGit/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(120, 113)
+        Me.Label1.Location = New System.Drawing.Point(69, 68)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(33, 13)
         Me.Label1.TabIndex = 0
